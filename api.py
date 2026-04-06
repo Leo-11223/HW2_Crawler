56bff7cb43f897122c2c4a6344bc2e698e1bee9d
Leo-11223 <leo04130506@gmail.com>
2026-04-06 13:35:27 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# 桃園市公車站牌資料 API
+
 url = "https://data.tycg.gov.tw/api/v1/rest/datastore/86364028-2f1d-44a6-9812-3211516e88e2?format=json"
 
 def get_data():
@@ -9,7 +9,7 @@ def get_data():
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            # 儲存成 JSON 檔案符合作業要求 [cite: 4466]
+            
             with open('taoyuan_bus.json', 'w', encoding='utf-8') as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)
             print("資料抓取成功並已儲存為 taoyuan_bus.json")
